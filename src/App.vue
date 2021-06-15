ef475ffbc048232bf9106fb19eb4037cfc626120
robertoshimizu <rshimizubr@gmail.com>
2021-06-14 21:45:39 -0300

    add first tailwind ui

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 /* eslint-disable vue/valid-template-root */
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Hero/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Hero from './components/Hero.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Hero
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
