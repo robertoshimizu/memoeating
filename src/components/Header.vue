77bd2a9f03149b54611d45d24bdb59204988f813
robertoshimizu <rshimizubr@gmail.com>
2021-06-14 22:53:25 -0300

    build files

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-indigo-600">
+  <header class="bg-indigo-600">
     <nav class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
       <div class="w-full py-6 flex items-center justify-between border-b border-indigo-500 lg:border-none">
         <div class="flex items-center">
@@ -21,7 +21,7 @@
         </a>
       </div>
     </nav>
-  </div>
+  </header>
 </template>
 
 <script>
