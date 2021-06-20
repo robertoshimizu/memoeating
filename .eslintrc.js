cd2c56c74d93b245d768f0b07edd24d22173162c
robertoshimizu <rshimizubr@gmail.com>
2021-06-20 14:26:24 +0000

    packages updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,21 +3,27 @@ module.exports = {
         "es6": true,
         "node": true
     },
-    "extends": "plugin:vue/essential",
+    "extends": [
+        "plugin:vue/essential",
+        'plugin:prettier/recommended',
+        '@vue/prettier'
+
+    ],
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
     },
     "parserOptions": {
         "ecmaVersion": 2018,
-        "sourceType": "module"
+        "sourceType": "module",
+        "parser": 'babel-eslint'
     },
     "plugins": [
         "vue"
     ],
     "rules": {
         "vue/no-multiple-template-root": "off",
-        "vue/valid-template-root":"off",
-        'vue/experimental-script-setup-vars':"off"
+        "vue/valid-template-root": "off",
+        'vue/experimental-script-setup-vars': "off"
     }
 };
