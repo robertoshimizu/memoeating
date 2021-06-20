72271b63584788152cbb9b6f769ba5f8a71108a3
robertoshimizu <rshimizubr@gmail.com>
2021-06-20 15:22:44 +0000

    Hero, team and blog

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,21 +1,106 @@
 <template>
-  <div class="relative bg-gray-50"> 
+  <div class="relative bg-gray-50">
     <main class="lg:relative">
-      <div class="mx-auto max-w-7xl w-full pt-16 pb-20 text-center lg:py-48 lg:text-left">
+      <div
+        class="
+          mx-auto
+          max-w-7xl
+          w-full
+          pt-16
+          pb-20
+          text-center
+          lg:py-48
+          lg:text-left
+        "
+      >
         <div class="px-4 lg:w-1/2 sm:px-8 xl:pr-16">
-          <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
-            <span class="block xl:inline">Data to enrich your</span>
-            {{ ' ' }}
-            <span class="block text-indigo-600 xl:inline">online business</span>
+          <h1
+            class="
+              text-4xl
+              tracking-tight
+              font-extrabold
+              text-gray-900
+              sm:text-5xl
+              md:text-6xl
+              lg:text-5xl
+              xl:text-6xl
+            "
+          >
+            <span class="block xl:inline">Construção de </span>
+            {{ " " }}
+            <span class="block text-indigo-600 xl:inline"
+              >experiências memoráveis</span
+            >
+            {{ " " }}
+            <span class="block xl:inline">no food service</span>
+            {{ " " }}
           </h1>
-          <p class="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">
-            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+          <p
+            class="
+              mt-3
+              max-w-md
+              mx-auto
+              text-lg text-gray-500
+              sm:text-xl
+              md:mt-5
+              md:max-w-3xl
+            "
+          >
+            A MEMO EATING oferece serviços especializados em Pesquisa &
+            Desenvolvimento de Produto, Qualidade e Análise Sensorial para o
+            food service.
           </p>
-          
+          <p
+            class="
+              mt-3
+              max-w-md
+              mx-auto
+              text-lg text-gray-500
+              sm:text-xl
+              md:mt-5
+              md:max-w-3xl
+            "
+          >
+            Atua na interface entre a indústria de alimentos e operadores de
+            restaurantes, ajudando os negócios na construção de experiências de
+            consumo afetivas e positivamente memoráveis.
+          </p>
+          <p
+            class="
+              mt-3
+              max-w-md
+              mx-auto
+              text-lg text-gray-500
+              sm:text-xl
+              md:mt-5
+              md:max-w-3xl
+            "
+          >
+            Através de um olhar holístico no food service, acreditamos que a
+            entrega do PRODUTO CERTO com EXCELÊNCIA NA QUALIDADE é um dos
+            importantes pilares desta construção.
+          </p>
         </div>
       </div>
-      <div class="relative w-full h-64 sm:h-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:h-full">
-        <img class="absolute inset-0 w-full h-full object-cover" src="https://images.unsplash.com/photo-1520333789090-1afc82db536a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="" />
+      <div
+        class="
+          relative
+          w-full
+          h-64
+          sm:h-72
+          md:h-96
+          lg:absolute
+          lg:inset-y-0
+          lg:right-0
+          lg:w-1/2
+          lg:h-full
+        "
+      >
+        <img
+          class="absolute inset-0 w-full h-full object-cover"
+          src="https://images.unsplash.com/photo-1564758596018-3e5b1f2340cc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+          alt=""
+        />
       </div>
     </main>
   </div>
@@ -24,6 +109,6 @@
 <script>
 export default {
   name: "Hero",
-}
+};
 </script>
 
